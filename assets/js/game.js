66cb1249f07a16e6ab17dfac41015cc52c223545
Sean Michael Shea <seanshea1337@gmail.com>
2019-09-15 14:31:21 -0400

    point system function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,3 @@
-
-// create countdown function
-
 function countdown() {
     var counter = 60;
     var timeCountdown = setInterval(function(){
@@ -10,6 +7,7 @@ function countdown() {
       if (counter === 0) {
         clearInterval(timeCountdown);
         $('.timer-container .time').html("Times Up");
+        points();
       }
     }, 1000);
 
@@ -17,62 +15,71 @@ function countdown() {
 }
 
 
-    let trivia = [
-        {
-            question: "How many wheels are on a skateboard?",
-            choices: ["2",  "4", "6", "8"],
-            answer: "2",
-        },
-        {
-            question: "Who invented the kickflip?",
-            choices: ["Tony Hawk", "Bam Magera", "Rodney Mullen", "Chad Muska"],
-            answer: "Rodney Mullen"
-        },
-        {
-            question: "Who did the first 900?",
-            choices: ["Tony Hawk", "Tas Pappas", "Danny Way", "bob burnquist"],
-            answer: "Tony Hawk",
-        },
-        {
-            question: "What is another word for a 360 flip?",
-            choices: ["Impossible Flip", "3 1/2 flip", "Tre Bomb", "Tri Flip"],
-            answer: "Tre Bomb",
-        }
-    ];
+let trivia = [
+    {
+        question: "How many wheels are on a skateboard?",
+        choices: ["2",  "4", "6", "8"],
+        answer: "2",
+    },
+    {
+        question: "Who invented the kickflip?",
+        choices: ["Tony Hawk", "Bam Magera", "Rodney Mullen", "Chad Muska"],
+        answer: "Rodney Mullen"
+    },
+    {
+        question: "Who did the first 900?",
+        choices: ["Tony Hawk", "Tas Pappas", "Danny Way", "bob burnquist"],
+        answer: "Tony Hawk",
+    },
+    {
+        question: "What is another word for a 360 flip?",
+        choices: ["Impossible Flip", "3 1/2 flip", "Tre Bomb", "Tri Flip"],
+        answer: "Tre Bomb",
+    }
+];
 
 
 function triviaQuestions() {
-    for(i= 0; i < trivia.length; i++) {        
-        var questionHeader = $('<h2 class="test">');
+    for(var i = 0; i < trivia.length; i++) {   
+    
+        var questionHeader = $('<h2 class="question-' + i + '">');
         var questionHeaderContent = questionHeader.text(trivia[i].question);
-        $('.question-container').append(questionHeaderContent);
-        console.log(trivia[i].choices[i]);
-      }
+        $('.question-container').append(questionHeaderContent).append("<form class='choices choices-container-" + i + "' id='choices-" + i + "'>");
 
-      
+        for (var j = 0; j < trivia.length; j++) {
+            console.log(trivia[i].choices[j]);
+            var questionChoices = $('<input type="radio"' + 'name="'  + i + '"'+ 'value="' + trivia[i].choices[j] + '">' + '<label>' + trivia[i].choices[j] + '</label>');
+            var questionChoicesContent = questionChoices.text(trivia[i].choices[j]);
+            $('.choices-container-' + i).append(questionChoices).append(questionChoicesContent);
+
+        }
+    }  
+    
 }
 
-function trivaChoices() {
- 
-    trivia .forEach(function (outerObj) {
-        Object.keys(outerObj).forEach(function (key) {
-          outerObj[key].forEach(function (item) {
-            console.log(item);
-          });
-        });
-      });
+function points() {
+    var correct = 0;
+    var incorrect = 0;
+    $(".choices").each(function(i){
+        var questionid = $(this).attr('id').split('-')[1];
+        var answer = $(this).find("input:checked").val();
+        if (answer == trivia[questionid].answer) {
+            correct += 1;
+        } else {
+            incorrect += 1;
+        }
+    });
+    $(".points-container").toggle();
+    $(".points-container .correct-answers").text(correct);
+    $(".points-container .incorrect-answers").text(incorrect);
 }
+
 
 $( document ).ready(function() {
     $('.start-button').on('click', function() {
         $(this).toggle();
         countdown();
         triviaQuestions();
-        trivaChoices();
-        
     });
-
-
-   
     
 });
